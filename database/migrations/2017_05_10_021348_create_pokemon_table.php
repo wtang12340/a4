95fd0cc045600c7c18ad56d23ffcb22385fbd550
William Tang <wtang12340@gmail.com>
2017-05-10 21:49:12 -0400

    Migrations and Seeders

---
+++ database/migrations/2017_05_10_021348_create_pokemon_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePokemonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('monsters', function (Blueprint $table) {
+
+
+		$table->increments('id');
+		$table->timestamps();
+
+
+		$table->string('name');
+		$table->string('type1');
+		$table->string('type2')->nullable();
+		$table->integer('level');
+		$table->string('trainer');
+
+
+	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('monsters');
+    }
+}
---
+++ database/seeds/MonstersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Monster;
+
+class MonstersTableSeeder extends Seeder
+{
+    /**
+    * Run the database seeds.
+    *
+    * @return void
+    */
+    public function run()
+    {
+        //
+        Monster::insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'name' => 'Bulbasaur',
+            'type1' => 'Grass',
+            'type2' => 'Poison',
+            'level' => 5,
+            'trainer' => 'Will',
+        ]);
+
+        Monster::insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'name' => 'Charmander',
+            'type1' => 'Fire',
+            'type2' => NULL,
+            'level' => 5,
+            'trainer' => 'Will',
+        ]);
+
+        Monster::insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'name' => 'Squirtle',
+            'type1' => 'Water',
+            'type2' => NULL,
+            'level' => 5,
+            'trainer' => 'Will',
+        ]);
+
+    }
+}
